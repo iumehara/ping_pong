cd1d83ab52a16e1b665a777425d5c6f8b34dce1b
iumehara <iumehara10@gmail.com>
2016-10-16 15:40:39 +0900

    adds status to tournaments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015111144) do
+ActiveRecord::Schema.define(version: 20161016060817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20161015111144) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "status"
   end
 
 end
